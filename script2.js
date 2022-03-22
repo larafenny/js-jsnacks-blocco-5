d92ab7649b2811b42362fdb4b2d536fe7c2d643c
Lara Filippone <lara.filippone@gmail.com>
2022-03-22 16:57:31 +0100

    modified length property in zucchineArray

--- script2.js
+++ script2.js
@@ -7,51 +7,51 @@ const zucchineArray = [
     {
         varieta: 'a',
         peso: 1,
-        lunghezza: 2,
+        lunghezza: 21,
     },
     {
         varieta: 'b',
         peso: 1.1,
-        lunghezza: 2.1,
+        lunghezza: 14,
     },
     {
         varieta: 'c',
         peso: 1.2,
-        lunghezza: 2.2,
+        lunghezza: 30,
     },
     {
         varieta: 'd',
         peso: 1.3,
-        lunghezza: 2.3,
+        lunghezza: 27,
     },
     {
         varieta: 'e',
         peso: 1.4,
-        lunghezza: 2.4,
+        lunghezza: 12,
     },
     {
         varieta: 'f',
         peso: 1.5,
-        lunghezza: 2.5,
+        lunghezza: 4,
     },
     {
         varieta: 'g',
         peso: 1.6,
-        lunghezza: 2.6,
+        lunghezza: 20,
     },
     {
         varieta: 'h',
         peso: 1.7,
-        lunghezza: 2.7,
+        lunghezza: 7,
     },
     {
         varieta: 'k',
         peso: 1.8,
-        lunghezza: 2.8,
+        lunghezza: 16,
     },
     {
         varieta: 'i',
         peso: 1.9,
-        lunghezza: 2.9,
+        lunghezza: 15,
     },
 ];

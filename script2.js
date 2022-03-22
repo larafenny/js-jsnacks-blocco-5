2350b3f39d449f06447658ff99762b8aafa847bd
Lara Filippone <lara.filippone@gmail.com>
2022-03-22 16:54:42 +0100

    created array wiith 10 zucchine

--- script2.js
+++ script2.js
@@ -1,3 +1,57 @@
 console.log('JS ok');
 
-/* Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine./*
+/* Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.*/
+
+// Creo array che contiene 10 elementi
+const zucchineArray = [
+    {
+        varieta: 'a',
+        peso: 1,
+        lunghezza: 2,
+    },
+    {
+        varieta: 'b',
+        peso: 1.1,
+        lunghezza: 2.1,
+    },
+    {
+        varieta: 'c',
+        peso: 1.2,
+        lunghezza: 2.2,
+    },
+    {
+        varieta: 'd',
+        peso: 1.3,
+        lunghezza: 2.3,
+    },
+    {
+        varieta: 'e',
+        peso: 1.4,
+        lunghezza: 2.4,
+    },
+    {
+        varieta: 'f',
+        peso: 1.5,
+        lunghezza: 2.5,
+    },
+    {
+        varieta: 'g',
+        peso: 1.6,
+        lunghezza: 2.6,
+    },
+    {
+        varieta: 'h',
+        peso: 1.7,
+        lunghezza: 2.7,
+    },
+    {
+        varieta: 'k',
+        peso: 1.8,
+        lunghezza: 2.8,
+    },
+    {
+        varieta: 'i',
+        peso: 1.9,
+        lunghezza: 2.9,
+    },
+];
